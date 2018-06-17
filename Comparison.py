32461265ab5adb7c678293a6ea297a3031349a63
losblancos13 <mayuresh18@DESKTOP-DLF4SLC.localdomain>
2018-06-18 00:11:13 +0530

    Comparison of two numbers Checking if the number is even or odd Getting an input within specified range

---
+++ Comparison.py
@@ -0,0 +1,8 @@
+Input1,Input2=eval(input("Enter two numbers: "))
+if Input1==Input2: #checks if both the entered numbers are equal
+        print("Both the inputs are equal.")
+elif Input1>Input2: #to check which of the input is greater
+        print("Input1 is greater than Input2.")
+else:
+        print("Input1 is lesser than Input2.")
+
