d56bd5adee26b835171422169ceb12bdb83ae9c3
losblancos13 <mayuresh18@DESKTOP-DLF4SLC.localdomain>
2018-06-17 23:52:41 +0530

    Centigrade to Fahrenheit Conversion

---
+++ CtoF.py
@@ -0,0 +1,5 @@
+Celsius=eval(input("Enter temperature in Degree Celsius: "))
+Fahrenheit=(Celsius*9/5)+32
+print(Celsius," degree celsius is equal to ",Fahrenheit," degree Fahrenheit")
+type(Fahrenheit)
+
