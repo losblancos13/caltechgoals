b5f3e3791641a8ca7ebb23115690eec1ea0301b8
losblancos13 <mayuresh18@DESKTOP-DLF4SLC.localdomain>
2018-06-18 00:30:42 +0530

    getting input in specified range

---
+++ InputInRange.py
@@ -0,0 +1,8 @@
+
+i=eval(input("Enter your input: "))
+inp=i
+while (9>inp) or (inp>79):
+        print("Input not in range.")
+        inp=eval(input("Enter your input: "))
+print(inp," is the input in specified range")
+
