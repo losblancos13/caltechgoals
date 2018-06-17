7d32abe3951fbcdec56bdf50e0509671d0fd4d65
losblancos13 <mayuresh18@DESKTOP-DLF4SLC.localdomain>
2018-06-17 23:41:59 +0530

    Average of numbers entered

---
+++ average.py
@@ -0,0 +1,10 @@
+count=eval(input("Enter how many numbers do you want an average of: "))
+a=0
+for i in range(count):
+        b=eval(input("Enter a number: "))
+        a=a+b
+Average=a/count
+print("The average of given numbers is: ",Average)
+        
+        
+
