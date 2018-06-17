bc10735f286d02bbb9c1eeb1b0177a4f3b7c02de
losblancos13 <mayuresh18@DESKTOP-DLF4SLC.localdomain>
2018-06-18 00:24:50 +0530

    Month of the year

--- InputInTange.py
+++
@@ -1,8 +0,0 @@
-
-i=eval(input("Enter your input: "))
-inp=i
-while (9>inp) or (inp>79):
-        print("Input not in range.")
-        inp=eval(input("Enter your input: "))
-print(inp," is the input in specified range")
-
---
+++ month.py
@@ -0,0 +1,26 @@
+m=eval(input("Enter a number between 1-12: "))
+if m==1:
+        print(m," corresponds to January.")
+if m==2:
+        print(m," corresponds to February.")
+if m==3:
+        print(m," corresponds to March.")
+if m==4:
+        print(m," corresponds to April.")
+if m==5:
+        print(m," corresponds to May.")
+if m==6:
+        print(m," corresponds to June.")
+if m==7:
+        print(m," corresponds to July.")
+if m==8:
+        print(m," corresponds to August.")
+if m==9:
+        print(m," corresponds to September.")
+if m==10:
+        print(m," corresponds to October.")
+if m==11:
+        print(m," corresponds to November.")
+if m==12:
+        print(m," corresponds to December.")
+
