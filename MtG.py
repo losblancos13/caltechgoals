1c77f32629c5b0958ffd146297ec5fcfc83f00ed
losblancos13 <mayuresh18@DESKTOP-DLF4SLC.localdomain>
2018-06-18 00:45:02 +0530

    Convert Marks to Grades

---
+++ MtG.py
@@ -0,0 +1,17 @@
+m=eval(input("Enter your Marks: "))
+if m>=90:
+        print("Your grade is O")
+if 80<=m<=89:
+        print("Your grade is A")
+if 70<=m<=79:
+        print("Your grade is B")
+if 60<=m<=69:
+        print("Your grade is C")
+if 50<=m<=59:
+        print("Your grade is D")
+if 40<=m<=49:
+        print("Your grade is E")
+if m<40:
+        print("You have failed")
+        
+
